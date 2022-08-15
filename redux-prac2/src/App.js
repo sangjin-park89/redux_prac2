f5f6a61eb506e22486af12c9738394a4d4ac053a
sangjin <sasangjin@gmail.com>
2022-08-15 11:26:45 +0900

    add number

--- redux-prac2/src/App.js
+++ redux-prac2/src/App.js
@@ -1,29 +1,51 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useState } from "react";
+
+import { minusOne, plusOne, addNumber, minNumber } from "./redux/modules/counter";
 
 function App() {
   const dispatch = useDispatch();
+  const [number, setNumber] = useState(0);
+  const globalNumber = useSelector((state) => state.counter.number);
 
-  const number = useSelector((state) => state.counter.number);
-  console.log(number);
+  const onChangeHandler = (event) => {
+    const { value } = event.target;
+    setNumber(+value);
+  };
+
+  const onClickAddNumberHandler = () => {
+    dispatch(addNumber(number))
+  } 
+
+  const onClickMinNumberHandler = () => {
+    dispatch(minNumber(number))
+  }
+  console.log(number)
+
+  // const number = useSelector((state) => state.counter.number);
 
   return (
     <div>
-      {number}
+      <h1>{number}</h1>
+      <h2>{globalNumber}</h2>
       <button
         onClick={() => {
-          dispatch({ type: "PLUS_ONE" });
+          dispatch(plusOne());
         }}
       >
         + 1
       </button>
       <button
         onClick={() => {
-          dispatch({ type: "MINUS_ONE" });
+          dispatch(minusOne());
         }}
       >
         - 1
       </button>
+      <input type="number" onChange={onChangeHandler}></input>
+      <button onClick={onClickAddNumberHandler}>더하기</button>
+      <button onClick={onClickMinNumberHandler}>빼기</button>
     </div>
   );
 }
--- redux-prac2/src/redux/modules/counter.js
+++ redux-prac2/src/redux/modules/counter.js
@@ -1,17 +1,55 @@
+const PLUS_ONE = "PLUS_ONE";
+const MINUS_ONE = "MINUS_ONE";
+const ADD_NUMBER = "ADD_NUMBER";
+const MIN_NUMBER = "MIN_NUMBER";
+
+export const plusOne = () => {
+    return {
+        type: PLUS_ONE,
+    };
+};
+
+export const minusOne = () => {
+    return {
+        type: MINUS_ONE,
+    };
+};
+
+export const addNumber = (payload) => {
+    return {
+        type: ADD_NUMBER,
+        payload,
+    };
+};
+
+export const minNumber = (payload) => {
+    return {
+        type: MIN_NUMBER,
+        payload,
+    };
+};
+
 const initialState = {
     number: 0,
 };
 
 const counter = (state = initialState, action) => {
-    console.log(action);
     switch (action.type) {
-        case "PLUS_ONE":
+        case PLUS_ONE:
             return {
                 number: state.number + 1,
             };
-        case "MINUS_ONE":
+        case MINUS_ONE:
             return {
                 number: state.number - 1,
+            };
+        case ADD_NUMBER:
+            return {
+                number: state.number + action.payload,
+            };
+        case MIN_NUMBER:
+            return {
+                number: state.number - action.payload,
             };
 
         default:
